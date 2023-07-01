9e3d2c6c74585dedc0b34ecb2484c40c807ed3c1
Yangui77 <fuditphuguillaume@gmail.com>
2023-07-01 17:16:40 +0200

    test: solve three of a kind

--- src/main/java/org/yatze/Yatzy.java
+++ src/main/java/org/yatze/Yatzy.java
@@ -93,6 +93,19 @@ public class Yatzy {
     }
 
     public int threeOfAKind() {
-        return 12;
+        Map<Integer, Integer> duplicatesCount = new HashMap<>();
+        Arrays.stream(values).forEach(
+                value -> {
+                    if (duplicatesCount.containsKey(value)) {
+                        duplicatesCount.put(value, duplicatesCount.get(value) + 1);
+                    } else {
+                        duplicatesCount.put(value, 1);
+                    }
+                }
+        );
+        Optional<Map.Entry<Integer, Integer>> result = duplicatesCount.entrySet().stream().filter(
+                integerIntegerEntry -> integerIntegerEntry.getValue() == 3
+        ).findFirst();
+        return result.map(integerIntegerEntry -> integerIntegerEntry.getKey() * 3).orElse(0);
     }
 }
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -125,7 +125,8 @@ public class YatzyTest {
 
     @ParameterizedTest
     @CsvSource({
-            "4, 4, 4, 2, 6, 12"
+            "4, 4, 4, 2, 6, 12",
+            "3, 3, 3, 5, 5, 9"
     })
     void shouldGetValidResultForThreeOfAKind(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).threeOfAKind());
