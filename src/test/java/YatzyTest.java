64b065554d1f76984eaeceb271706ec2aafc2067
Yangui77 <fuditphuguillaume@gmail.com>
2023-07-01 17:55:34 +0200

    test: add tests

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -185,7 +185,12 @@ public class YatzyTest {
     @ParameterizedTest
     @CsvSource({
             "3, 3, 3, 2, 2, 13",
-            "4, 4, 4, 4, 4, 0"
+            "4, 4, 4, 4, 4, 0",
+            "3, 3, 2, 2, 2, 12",
+            "5, 5, 5, 5, 1, 0",
+            "5, 5, 5, 6, 6, 27",
+            "6, 6, 6, 1, 1, 20",
+            "2, 2, 2, 1, 1, 8"
     })
     void shouldGetValidResultForFullHouse(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertEquals(expected, new Yatzy(d1, d2, d3, d4, d5).fullHouse());
