0ef6e86d243936608d6434a4a92b14d13a601c22
Yangui77 <fuditphuguillaume@gmail.com>
2023-07-01 15:38:50 +0200

    test: add test cases for one

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -21,7 +21,12 @@ public class YatzyTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1, 2, 3, 4, 5, 1"
+            "1, 2, 3, 4, 5, 1",
+            "1, 1, 1, 2, 2, 3",
+            "1, 1, 2, 3, 3, 2",
+            "2, 2, 3, 4, 5, 0",
+            "1, 1, 1, 1, 1, 5",
+            "2, 3, 4, 1, 1, 2"
     })
     void shouldGetValidResultForOnes(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertEquals(expected, Yatzy.ones(d1, d2, d3, d4, d5));
