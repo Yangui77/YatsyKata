a1805ff48b4d881ee9f39870fee05549e7814727
Yangui77 <fuditphuguillaume@gmail.com>
2023-07-01 15:41:16 +0200

    refacto: small refacto for sum

--- src/main/java/org/yatze/Yatzy.java
+++ src/main/java/org/yatze/Yatzy.java
@@ -3,41 +3,29 @@ package org.yatze;
 public class Yatzy {
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) {
-            sum += 2;
-        }
-        if (d2 == 2) {
-            sum += 2;
-        }
-        if (d3 == 2) {
-            sum += 2;
-        }
-        if (d4 == 2) {
-            sum += 2;
-        }
-        if (d5 == 2) {
-            sum += 2;
-        }
-        return sum;
+        return sum(2, d1, d2, d3, d4, d5);
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
+        return sum(1, d1, d2, d3, d4, d5);
+    }
+
+    private static int sum(int value, int d1, int d2, int d3, int d4, int d5) {
         int sum = 0;
-        if (d1 == 1) {
-            sum += 1;
+        if (d1 == value) {
+            sum += value;
         }
-        if (d2 == 1) {
-            sum += 1;
+        if (d2 == value) {
+            sum += value;
         }
-        if (d3 == 1) {
-            sum += 1;
+        if (d3 == value) {
+            sum += value;
         }
-        if (d4 == 1) {
-            sum += 1;
+        if (d4 == value) {
+            sum += value;
         }
-        if (d5 == 1) {
-            sum += 1;
+        if (d5 == value) {
+            sum += value;
         }
         return sum;
     }
