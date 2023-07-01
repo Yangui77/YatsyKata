376e8ef555bd88f9d06b7fe6a0173c1f1ee3c9ba
Yangui77 <fuditphuguillaume@gmail.com>
2023-07-01 15:34:57 +0200

    test: add new test case

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.yatze.Yatzy;
@@ -10,7 +9,11 @@ public class YatzyTest {
     @ParameterizedTest
     @CsvSource({
             "2, 2, 3, 4, 5, 4",
-            "2, 2, 2, 4, 5, 6"
+            "2, 2, 2, 4, 5, 6",
+            "1, 1, 1, 1, 1, 0",
+            "2, 5, 5, 5, 5, 2",
+            "5, 5, 5, 1, 2, 2",
+            "1, 1, 1, 1, 1, 0"
     })
     void shouldGetValidResultForTwos(int d1, int d2, int d3, int d4, int d5, int expected) {
         assertEquals(expected, Yatzy.twos(d1, d2, d3, d4, d5));
